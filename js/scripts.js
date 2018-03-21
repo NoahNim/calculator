86c99e6ed3f53fe37c6112e351c4c696e1715dc6
Eric Wolfe and Noah Medoff <eric@>
2018-03-21 10:41:30 -0700

    complete math functions and web form

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,61 @@
 function add(num1, num2){
 return num1+ num2;
-}
+};
 
 function subtract(num1, num2){
   return num1 - num2;
-}
+};
 
 function multiply(num1, num2){
   return num1 * num2;
-}
+};
 
 function divide(num1, num2){
   return num1 / num2;
-}
+};
 
-var num1 = parseInt(prompt("Enter a number please"));
-var num2 = parseInt(prompt("Enter another number please"));
-alert(add(num1,num2));
-alert(subtract(num1, num2));
-alert(multiply(num1, num2));
-alert(divide(num1, num2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    console.log(number1);
+    var number2 = parseInt($("#add2").val());
+    console.log(number2);
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    console.log(number1);
+    var number2 = parseInt($("#sub2").val());
+    console.log(number2);
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    console.log(number1);
+    var number2 = parseInt($("#mult2").val());
+    console.log(number2);
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    console.log(number1);
+    var number2 = parseInt($("#div2").val());
+    console.log(number2);
+    var result = divide(number1, number2);
+    $("#output").text(result);
+
+  });
+
+});
